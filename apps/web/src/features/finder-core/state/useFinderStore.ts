6c8adf19eb429dbcc80b57f252d06fd6d9da5c60
Stéphane Bazzé <stephane.bazze@outlook.fr>
2026-04-30 11:36:39 +0200

    feat(finder-core): add zustand store for navigation and selection

---
+++ apps/web/src/features/finder-core/state/useFinderStore.ts
@@ -0,0 +1,76 @@
+import { create } from "zustand";
+import type { FinderNode } from "@features/finder-core/types";
+
+type SelectionMode = 'single' | 'multiple';
+
+interface FinderState {
+  // navigation
+  currentPath: string;
+  setPath: (path: string) => void;
+
+  // currennt content
+  folders: FinderNode[];
+  files: FinderNode[];
+  setContent: (data: { folders: FinderNode[]; files: FinderNode[] }) => void;
+
+  // selection
+  selectionMode: SelectionMode;
+  selected: Set<string>;
+
+  select: (id: string) => void;
+  toggleSelect: (id: string) => void;
+  clearSelection: () => void;
+
+  setSelectionMode: (mode: SelectionMode) => void;
+};
+
+export const useFinderStore = create<FinderState>((set, get) => ({
+  // navigation
+  currentPath: '/',
+  setPath: (path) => set({ currentPath: path }),
+
+  // content
+  folders: [],
+  files: [],
+  setContent: ({ folders, files }) => set({ folders, files }),
+
+  // selection
+  selectionMode: 'single',
+  selected: new Set(),
+
+  select: (id) => {
+    const { selectionMode } = get();
+
+    if (selectionMode === 'single') {
+      set({ selected: new Set([id]) });
+    } else {
+      set((state) => {
+        const next = new Set(state.selected);
+        next.add(id);
+        return { selected: next };
+      });
+    }
+  },
+
+  toggleSelect: (id) => {
+    set((state) => {
+      const next = new Set(state.selected);
+
+      if (next.has(id)) {
+        next.delete(id);
+      } else {
+        if (state.selectionMode === 'single') {
+          return { selected: new Set([id]) };
+        }
+        next.add(id);
+      }
+
+      return { selected: next };
+    });
+  },
+
+  clearSelection: () => set({ selected: new Set() }),
+
+  setSelectionMode: (mode) => set({ selectionMode: mode }),
+}));
+
